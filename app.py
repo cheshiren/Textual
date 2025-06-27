ca11364a3bbd170c3ab957c9ae533bd810210925
Vyacheslav Dobranov <vy.dobranov@yandex.ru>
2025-06-28 01:22:45 +0500

    Changes to be committed: 	new file:   __pycache__/texts.cpython-312.pyc 	modified:   app.py 	modified:   test.tcss 	modified:   texts.py 	modified:   textual.log

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ import random
 from textual import events
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical, Grid, Container, Center
+from textual.content import Content
 from textual.message import Message
 from textual.screen import Screen
 from textual.theme import Theme
 from textual.widgets import Button, Header, Footer, Placeholder, Markdown, Link, Static, Label
 
-from texts import Cut, Photos
+from texts import Cut, Photos, Photo
 
 TITLE = "Поход за покупками в Лихой Город"
 AUTHOR = "Вячеслав Добранов"
@@ -33,18 +34,21 @@ reckless_theme = Theme(
 	},
 )
 
+
 class PhotoButton(Button):
-	
+
 	def action_press(self):
 		self.add_class("seen")
-	
+
 	async def on_click(self) -> None:
 		await self.run_action("press()")
 
+
 class UnfocusedScreen(Screen):
-	
+
 	def on_show(self, event):
 		self.set_focus(None)
+
 
 class StartScreen(UnfocusedScreen):
 
@@ -55,7 +59,7 @@ class StartScreen(UnfocusedScreen):
 			yield Label(AUTHOR, id="author-start-screen")
 			yield Button("Начать игру", classes="blur")
 		# yield Footer()
-	
+
 	class StartPressed(Message):
 		pass
 
@@ -63,19 +67,20 @@ class StartScreen(UnfocusedScreen):
 		event.stop()
 		self.post_message(self.StartPressed())
 
+
 class CutScreen(UnfocusedScreen):
-	
-	def __init__(self, name = None, id = None, classes = None, text: str = "CUTSCENE", next_screen: str = "start_screen"):
+
+	def __init__(self, name=None, id=None, classes=None, text: str = "CUTSCENE", next_screen: str = "start_screen"):
 		super().__init__(name, id, classes)
 		self.text = text
 		self.next_screen = next_screen
-	
+
 	def compose(self):
 		yield Header()
 		with Vertical():
 			yield Static(self.text)
 			yield Center(Button("››"))
-	
+
 	class NextPressed(Message):
 		def __init__(self, next_screen: str):
 			super().__init__()
@@ -86,24 +91,35 @@ class CutScreen(UnfocusedScreen):
 		self.post_message(self.NextPressed(self.next_screen))
 
 
-class PhotoScreen(UnfocusedScreen):
+class PhotosScreen(UnfocusedScreen):
 
-	def __init__(self, name = None, id = None, classes = None, labels: list[str] = []):
+	def __init__(self, name=None, id=None, classes=None, photos: list[Photo] = []):
 		super().__init__(name, id, classes)
-		self.labels = labels
+		self.photos = photos
 
 	def compose(self) -> ComposeResult:
 		yield Header()
 		with Vertical(id="photo_column"):
 			# yield Static("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя")
 			with Grid(id="photo_panel"):
-				for i in range(len(self.labels)):
-					yield PhotoButton(self.labels[i], id=f"photo{i+1}")
+				for i in range(len(self.photos)):
+					yield PhotoButton(self.photos[i].label, id=f"photo{i+1}")
 		# yield Footer()
 
 	def on_mouse_move(self) -> None:
 		# print(event.offset)
 		self.query("PhotoButton").blur()
+
+
+class PhotoScreen(UnfocusedScreen):
+	def __init__(self, name=None, id=None, classes=None, photo: Photo = Photo()):
+		super().__init__(name, id, classes)
+		self.photo = photo
+	def compose(self):
+		yield Header()
+		with Vertical():
+			yield Static(Content(self.photo.description))
+			yield Center(Button("‹‹"))
 
 
 class TestApp(App):
@@ -112,31 +128,35 @@ class TestApp(App):
 	TITLE = TITLE
 	# LABELS = ["Рынок", "Ме́ха", "Не более 67%", "Розовые пантеры Джайпура", "Красные платки", "三足狗", "Café hidropônico", "Один-пятьдесят восемь", "Фарм-принтер"]
 	PHOTOS = Photos.photos
-	LABELS = [i.label for i in PHOTOS]
 
 	def on_mount(self) -> None:
 		# Register the theme
-		self.register_theme(reckless_theme)  
+		self.register_theme(reckless_theme)
 		# Set the app's theme
-		self.theme = "reckless"  
+		self.theme = "reckless"
 
-		random.shuffle(self.LABELS)
+		# random.shuffle(self.PHOTOS) # UNCOMMENTME
 		self.install_screen(StartScreen(), name="start_screen")
-		self.install_screen(CutScreen(text=Cut.intro_text, next_screen="photo_screen"), name="intro_screen")
+		self.install_screen(CutScreen(text=Cut.intro_text,
+							next_screen="photos_screen"), name="intro_screen")
 		self.install_screen(CutScreen(), name="interlude_screen")
 		self.install_screen(CutScreen(), name="outro_screen")
-		self.install_screen(PhotoScreen(labels=self.LABELS), name="photo_screen")
+		self.install_screen(PhotosScreen(
+			photos=self.PHOTOS), name="photos_screen")
+		for i in range(len(self.PHOTOS)):
+			self.install_screen(PhotoScreen(
+				photo=self.PHOTOS[i]), name=f"photo{i+1}_screen")
 
 		self.push_screen("start_screen")
-		self.push_screen("photo_screen")
+		self.push_screen("photos_screen") # DELETEME
+		self.push_screen("photo1_screen") # DELETEME
 
 	def on_start_screen_start_pressed(self, event: StartScreen.StartPressed):
 		self.push_screen("intro_screen")
-	
+
 	def on_cut_screen_next_pressed(self, event: CutScreen.NextPressed):
 		self.push_screen(event.next_screen)
 
-	
 
 EXAMPLE_MARKDOWN = """\
 # Markdown Document
@@ -163,6 +183,7 @@ Hello, [bold $text on $primary]World[/]!
 [@click=app.notify('Hello, World!')]Click me[/]
 """
 
+
 class MarkdownExampleApp(App):
 	CSS = """
 		Content Link {
@@ -176,10 +197,12 @@ class MarkdownExampleApp(App):
 			&:hover { color: $accent-lighten-1; }
 		}
 	"""
+
 	def compose(self) -> ComposeResult:
 		yield Link("ЖОПА!!!")
 		yield Static(TEXT1)
 		yield Markdown(EXAMPLE_MARKDOWN, id="md")
+
 
 if __name__ == "__main__":
 	app = TestApp()
