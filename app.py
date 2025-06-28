2d3ccde81a415569716eb17c3b2a480f416b89b1
Vyacheslav Dobranov <vy.dobranov@yandex.ru>
2025-06-28 23:06:50 +0500

    Changes to be committed: 	new file:   .gitignore 	modified:   app.py 	modified:   test.tcss 	modified:   texts.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical, Grid, Container, Center
 from textual.content import Content
 from textual.message import Message
-from textual.screen import Screen
+from textual.screen import Screen, ModalScreen
 from textual.theme import Theme
 from textual.widgets import Button, Header, Footer, Placeholder, Markdown, Link, Static, Label
 
-from texts import Cut, Photos, Photo
+from texts import Cut, Photos, Photo, Notes, Note, photo10
 
 TITLE = "Поход за покупками в Лихой Город"
 AUTHOR = "Вячеслав Добранов"
@@ -36,7 +36,7 @@ reckless_theme = Theme(
 
 
 class PhotoButton(Button):
-
+	
 	def action_press(self):
 		self.add_class("seen")
 
@@ -108,22 +108,44 @@ class PhotosScreen(UnfocusedScreen):
 
 	def on_mouse_move(self) -> None:
 		# print(event.offset)
-		self.query("PhotoButton").blur()
+		self.query("Button").blur()
+
+	def on_screen_resume(self):
+		# Если все фотокнопки были нажаты - убираем скрытый класс с кнопки Дальше
+		if len(self.query("PhotoButton.seen")) == len(self.photos):
+			self.query_one("#continue").remove_class("hidden")
 	
 	class PhotoPressed(Message):
 		def __init__(self, button_id: str|None):
 			super().__init__()
 			self.button_id = button_id
 
-	@on(PhotoButton.Pressed, ".photobutton")
-	def lets_see_the_photo(self, event: PhotoButton.Pressed):
+	def action_lets_see_the_photo(self, event: PhotoButton.Pressed):
 		button_id = event.button.id
-		# event.stop()
 		self.post_message(self.PhotoPressed(button_id))
 
-		# Если все фотокнопки были нажаты - убираем скрытый класс с кнопки Дальше
-		if len(self.query("PhotoButton.seen")) == len(self.photos):
-			self.query_one("#continue").remove_class("hidden")
+	@on(PhotoButton.Pressed, ".photobutton")
+	def lets_see_the_photo(self, event: PhotoButton.Pressed):
+		self.action_lets_see_the_photo(event)
+	
+	class ContinuePressed(Message):
+		pass
+
+	@on(Button.Pressed, "#continue")
+	def goto_interlude(self):
+		self.post_message(self.ContinuePressed())
+	
+	def key_enter(self):
+		try:
+			button = self.query_one("#continue")
+			if button.style.display != 'none':
+				self.post_message(self.ContinuePressed())
+		except:
+			pass
+		try:
+			button = self.query("PhotoButton:focus, PhotoButton:hover")[0]
+			self.action_lets_see_the_photo(Button.Pressed(button))
+		except: pass
 
 
 class PhotoScreen(UnfocusedScreen):
@@ -131,6 +153,7 @@ class PhotoScreen(UnfocusedScreen):
 		super().__init__(name, id, classes)
 		self.photo = photo
 	def compose(self):
+		app.TITLE = self.photo.label
 		yield Header()
 		with Vertical():
 			yield Static(Content(self.photo.description))
@@ -138,11 +161,69 @@ class PhotoScreen(UnfocusedScreen):
 	def on_mount(self):
 		if not self.photo.seen:
 			self.photo.first_seen()
+		self.query_one("Static").update(Content.from_markup(self.photo.description))
+		for l in self.query("Link"):
+			l.can_focus = True
 	class ReturnPressed(Message):
 		pass
 	def on_button_pressed(self, event: Button.Pressed):
 		event.stop()
 		self.post_message(self.ReturnPressed())
+	def key_backspace(self):
+		self.post_message(self.ReturnPressed())
+	
+class NoteScreen(ModalScreen):
+	def __init__(self, name = None, id = None, classes = None, note: Note = Note()):
+		super().__init__(name, id, classes)
+		self.note = note
+	def compose(self):
+		with Center():
+			with Container():
+				yield Static(self.note.text)
+				yield Center(Button("‹‹"))
+	def on_mount(self):
+		self.query_one(Container).border_title = self.note.label.upper()
+		self.query_one(Container).border_subtitle = "ИскИн — справка"
+	def on_button_pressed(self):
+		app.pop_screen()
+	def on_key(self):
+		app.pop_screen()
+
+class LastPhotoScreen(UnfocusedScreen):
+
+	def compose(self) -> ComposeResult:
+		yield Header()
+		with Vertical(id="photo_column"):
+			with Grid(id="photo_panel"):
+				yield Placeholder(classes="noopacity")
+				yield Placeholder(classes="noopacity")
+				yield Placeholder(classes="noopacity")
+				yield Placeholder(classes="noopacity")
+				yield PhotoButton(photo10.label, id="photo10", classes="photobutton")
+			yield Center(Button("››", id="continue", classes="hidden"))
+
+	def on_mouse_move(self) -> None:
+		# print(event.offset)
+		self.query("Button").blur()
+	
+	class PhotoPressed(Message):
+		def __init__(self, button_id: str|None):
+			super().__init__()
+			self.button_id = button_id
+
+	def action_lets_see_the_photo(self, event: PhotoButton.Pressed):
+		button_id = event.button.id
+		self.post_message(self.PhotoPressed(button_id))
+
+	@on(PhotoButton.Pressed, ".photobutton")
+	def lets_see_the_photo(self, event: PhotoButton.Pressed):
+		self.action_lets_see_the_photo(event)
+	
+	def key_enter(self):
+		try:
+			button = self.query("PhotoButton:focus, PhotoButton:hover")[0]
+			self.action_lets_see_the_photo(Button.Pressed(button))
+		except: pass
 
 
 class TestApp(App):
@@ -151,6 +232,7 @@ class TestApp(App):
 	TITLE = TITLE
 	# LABELS = ["Рынок", "Ме́ха", "Не более 67%", "Розовые пантеры Джайпура", "Красные платки", "三足狗", "Café hidropônico", "Один-пятьдесят восемь", "Фарм-принтер"]
 	PHOTOS = Photos.photos
+	NOTES = Notes.notes
 
 	def on_mount(self) -> None:
 		# Register the theme
@@ -158,21 +240,26 @@ class TestApp(App):
 		# Set the app's theme
 		self.theme = "reckless"
 
-		# random.shuffle(self.PHOTOS) # UNCOMMENTME
+		random.shuffle(self.PHOTOS) # UNCOMMENTME
 		self.install_screen(StartScreen(), name="start_screen")
 		self.install_screen(CutScreen(text=Cut.intro_text,
 							next_screen="photos_screen"), name="intro_screen")
-		self.install_screen(CutScreen(), name="interlude_screen")
+		self.install_screen(CutScreen(text=Cut.interlude_text, next_screen="last_photo_screen"), name="interlude_screen")
 		self.install_screen(CutScreen(), name="outro_screen")
 		self.install_screen(PhotosScreen(
 			photos=self.PHOTOS), name="photos_screen")
 		for i in range(len(self.PHOTOS)):
 			self.install_screen(PhotoScreen(
 				photo=self.PHOTOS[i]), name=f"photo{i+1}_screen")
+		self.install_screen(LastPhotoScreen(), name="last_photo_screen")
+		self.install_screen(PhotoScreen(photo=photo10), name="photo10_screen")
+		for n in self.NOTES:
+			self.install_screen(NoteScreen(
+				note=n), name=f"{n.name}_screen")
 
 		self.push_screen("start_screen")
 		self.push_screen("photos_screen") # DELETEME
-		# self.push_screen("photo1_screen") # DELETEME
+		self.push_screen("interlude_screen") # DELETEME
 
 	def on_start_screen_start_pressed(self, event: StartScreen.StartPressed):
 		self.push_screen("intro_screen")
@@ -183,8 +270,17 @@ class TestApp(App):
 	def on_photos_screen_photo_pressed(self, event: PhotosScreen.PhotoPressed):
 		self.push_screen(f"{event.button_id}_screen")
 
+	def on_last_photo_screen_photo_pressed(self, event: PhotosScreen.PhotoPressed):
+		self.push_screen(f"{event.button_id}_screen")
+
 	def on_photo_screen_return_pressed(self, event: PhotoScreen.ReturnPressed):
 		self.pop_screen()
+	
+	def action_note(self, note: str):
+		self.push_screen(f"{note}_screen")
+	
+	def on_photos_screen_continue_pressed(self):
+		self.push_screen("interlude_screen")
 
 
 EXAMPLE_MARKDOWN = """\
